c375f2ecba6c21639bfc279bacce8dc7ac262f12
Alex Price <alex@alexprice.co>
2017-09-16 17:13:37 -0500

    Adds Foursquare integration and ignores Notes file

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
+import { CLIENT_ID, CLIENT_SECRET } from '../config/foursquare-keys.js'
+import axios from 'axios'
 
 import base from '../components/general/rebase'
 import Loader from '../components/general/loader'
-import Selector from '../components/selector/selector'
 import Style from '../components/general/style'
 import sheet from '../components/base.scss'
 import Week from '../components/week/week'
@@ -12,28 +13,66 @@ export class Layout extends Component {
   constructor (props, context) {
     super(props, context)
     this.state = {
+      user: null,
       loading: true,
-      tacoPlaces: []
+      tacoPlaces: [
+        {
+          name: 'Koala Tacos',
+          price: 3
+        }
+      ]
     }
   }
 
   componentDidMount () {
-    this.updateData()
+    this.updateUserData('nguyenkimsagmailcom')
+    navigator.geolocation.getCurrentPosition(
+      this.updateTacoData,
+      (error) => {
+        console.warn('If running on localhost, ignore this.\nDefaulting to Capital Factory coordinates.\n', error)
+        if (error.code === 2) {
+          const position = {
+            coords: {
+              latitude: '30.269',
+              longitude: '-97.7406'
+            }
+          }
+
+          this.updateTacoData(position)
+        }
+      }
+    )
   }
 
-  updateDataFor (date) {
-    this.ref = base.fetch(`nguyenkimsagmailcom`, {
+  // pass in a user id, like `nguyenkimsagmailcom` to return all data for that user
+  updateUserData (userId) {
+    this.ref = base.fetch(userId, {
       context: this,
       asArray: false,
-      state: `timeline_${date}`,
+      state: `user`,
       then: (data) => {
-      	console.info(data)	
+        console.info(data)
         this.setState({
           loading: false,
-          [`timeline_${date}`]: data
+          user: data
         })
       }
     })
+  }
+
+  // pass in longitude and latitude to push all local taco places to state.
+  updateTacoData (position) {
+    const latitude = position.coords.latitude
+    const longitude = position.coords.longitude
+    const url = `https://api.foursquare.com/v2/venues/search?ll=${latitude},${longitude}&categoryId=4bf58dd8d48988d151941735&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=20170916`
+
+    axios.get(url).then((res) => {
+      console.debug(res)
+    }).catch((err) => {
+      console.warn(err)
+    })
+
+    // TODO: sort by deliciousness
   }
 
   renderSection (section) {
@@ -64,25 +103,32 @@ export class Layout extends Component {
   }
 
   render () {
-    return (
-      <main>
-        <header>
-          <h1>Gel</h1>
+    const {
+      user,
+      loading
+    } = this.state
 
-          <Selector
-            defaultOption={'Day'}
-            options={['Infinite', 'Week', 'Day']}
-            updateSelected={(option) => {
-              this.setState({ activeSection: option.toLowerCase() })
-            }}
-          />
+    if (loading) {
+      return (<main>LOADING</main>)
+    }
 
-          <Style sheet={sheet} />
-        </header>
-
-        { this.renderSection(this.state.activeSection) }
-      </main>
-    )
+    if (user) {
+      return (
+        <main>
+          <header>
+            <h1>Taco Quest</h1>
+            Kim really digs {user.favorite}.
+            <Style sheet={sheet} />
+          </header>
+        </main>
+      )
+    } else {
+      return (
+        <main>
+          LOCATION SCREEN
+        </main>
+      )
+    }
   }
 }
 
