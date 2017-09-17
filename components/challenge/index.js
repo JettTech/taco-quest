5a4cb9bec41e2771b4b2ca8334c09d2b1998dca2
divya <divyaxg@gmail.com>
2017-09-17 11:55:27 -0500

    challenge edits

--- components/challenge/index.js
+++ components/challenge/index.js
@@ -1,16 +1,23 @@
-import React, { Component } from 'react'
+import { Component } from 'react'
 
 export class Challenge extends Component {
-  constructor(props, contexts)
-  super(props, context)
-  this.state = {
-    sortedPlaces: [],
-    loading: true
+  constructor (props, context) {
+    super(props, context)
+    this.state = {
+      sortedPlaces: [],
+      loading: true
+    }
   }
-}
-getContenders (firstContender,secondContender)
-const last tacoPlaces.length-2
-const n = (Math.floor(Math.random()*last))
-  firstContender = tacoPlaces[n]
-  secondContender = tacoplaces[n+1]
+
+  getContenders () {
+    const { tacoPlaces } = this.props
+    const last = tacoPlaces.length - 2
+    const n = (Math.floor(Math.random() * last))
+    const firstContender = tacoPlaces[n]
+    const secondContender = tacoPlaces[n + 1]
+    return [firstContender, secondContender]
+  }
+  render (){
+
+  }
 }
