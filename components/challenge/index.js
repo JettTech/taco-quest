94526c17f294b4af4dc0df74e254b45fdd41469b
Lisa Jetton <ldjetton21@gmail.com>
2017-09-17 13:17:13 -0500

    some new things

--- components/challenge/index.js
+++ components/challenge/index.js
@@ -1,5 +1,6 @@
 import { Component } from 'react'
 import button from '../general/button.js'
+import ReactDOM from "react-dom";
 
 export class Challenge extends Component {
   constructor (props, context) {
@@ -10,6 +11,14 @@ export class Challenge extends Component {
     }
   }
 
+  class Input extends React.Component {
+    render() {
+        return (
+            <button value="Start" />
+        );
+    }
+}
+
   getContenders () {
     const { tacoPlaces } = this.props
     const last = tacoPlaces.length - 2
@@ -18,15 +27,80 @@ export class Challenge extends Component {
     const secondContender = tacoPlaces[n + 1]
     return [firstContender, secondContender]
   }
-  render (){
-    <section className=''>
-      <img/>
+
+
+class Form extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {inputList: []};
+        this.onAddBtnClick = this.onAddBtnClick.bind(this);
+    }
+
+    onAddBtnClick(event) {
+        const inputList = this.state.inputList; //add our inputs
+        this.setState({
+            inputList: inputList.concat(<Input key={inputList.length} />)
+        });
+    }
+
+  render() {
+        return (
+            <div>
+                <button onClick={this.onAddBtnClick}>Add input</button>
+                {this.state.inputList.map(function(input, index) {
+                    return {input}   
+                })}
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Form />,
+    document.getElementById("form")
+);
+  
+
+  startChallenge ()
+  const start = <button>
+    
+        <div onClick = {() => {
+  }}/>
+  
+  
+  //Rendering -> if tests begin..
+render (){
+    if (startChallenge) return startChallenge //create variable for Start Challenge
+    else () {
+}
+
+//code for the elements of New Challenge
+<section className='newChallenge'>
+    <image source ''/>  //how do you syntax???
       <p>
         "Will You Taco Down?"
       <p/>
-      <div className 'button'>
-
+    <div>
+<Button onClick={onClickAccept}
+  title="Accept"
+  accessibilityLabel="Accept the Taco Challenge by pressing this button"/>
       </div>
+     <div>   
+<Button onClick={onClickDeny}
+  title="Deny"
+  accessibilityLabel= "Deny the Taco Challenge by pressing this button" />
+     
+ </div>
     </section>
   }
 }
+
+ //code for the elements of Empty State
+
+  constructor (props, context) {
+    super(props, context)
+    this.state = {
+      sortedPlaces: [],
+      loading: true
+    }
+  }
