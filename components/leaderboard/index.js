918010840059e2b2a2f1d5dd5a16c3b71d7f4ada
divya <divyaxg@gmail.com>
2017-09-17 10:33:30 -0500

    cleaned up leaderboard

--- components/leaderboard/index.js
+++ components/leaderboard/index.js
@@ -1,67 +1,67 @@
-import React, { Component } from 'react'import React, { Component } from 'react'
-import Leaderboard from '../compnonets/leaderboard'
-
-//<Leaderboard tacoPlaces = {res from 4^2}/>
-//setState ({})
+import React, { Component } from 'react'
+import base from '../general/rebase'
+import Location from '../location'
 
 export class Leaderboard extends Component {
-	constructor (props, context) {
-		super (props, context)
-		this.state {
-			sortedPlaces: [],
-			loading: true
-		}
-	}
-}
+  constructor (props, context) {
+    super(props, context)
+    this.state = {
+      sortedPlaces: [],
+      loading: true
+    }
+  }
 
-async getLocationScore (locationId) {
-	const score await base.fetch('location-score/${locationId', {
-		context: this,
-		asArray: false,
-		then (reponse) => return response
-	})
-	if (score) return score
-	else //todo: set default in firebase 
-		//    --> return Default
-}
+  async getLocationScore (locationId) {
+    const score = await base.fetch(`location-score/${locationId}`, {
+      context: this,
+      asArray: false,
+      then: (response) => response
+    })
 
-componentWillReceiveProps (nextProps) {
-	if(this.state.loading && nextProps.tacoPlaces) {
-		const sortedTacoPlaces
-		this.setState ({
-			sortedPlaces: sortedTacoPlaces,
-			loading: false
-		})
-	}
-}
+    if (score) return score
+    else {
+      console.info('This Score Does Not Exist')
+    }
+  }
 
-render () {
-	const {
-		loading,
-		tacoPlaces
-	} = this.state
-}
+  componentWillReceiveProps (nextProps) {
+    if (this.state.loading && nextProps.tacoPlaces) {
+      const scoredPlaces = nextProps.tacoPlaces.map(place => {
+        let newPlace = place
+        newPlace.score = this.getLocationScore(place.id)
+        return newPlace
+      })
 
-if (loading) {
-	return ('<div>LOADING</div>') //Do we need ot include the single quotes for designated the html part?  ALSO.. which libraies / how does react.js use/know in order for it to render the html automatixcally.
-} else
-	const renderedTacoPlaces = tacoPlaces.map((place) => {
-		return <Location key = {place.id}
-			stars = {place.stars}
-			name = {place.name}
-			// distance = {...}
-		/>
-	})
-	return (
-		<section className = /'leaderboard'> //What is going on with the syntax here?!?!?!
-		/{renderedTacoPlaces}/
-		</section>
-	)
-}
+      const sortedPlaces = _.sortBy(scoredPlaces, ['score'])
 
-const scoredPlaces = tacoPlaces.map (place => {
-	let newPlace = place
-	newPlace = this.getLocationScore (placeId)
-	return newPlace
-})
-const sortedPlaces = _.sortBy (scoredPlaces, ['score'])
+      this.setState({
+        sortedPlaces,
+        loading: false
+      })
+    }
+  }
+
+  render () {
+    const {
+      loading,
+      tacoPlaces
+    } = this.state
+
+    if (loading) {
+      return (<div>LOADING</div>)
+    } else {
+      const renderedTacoPlaces = tacoPlaces.map((place) => {
+        return <Location key={place.id}
+          stars={place.stars}
+          name={place.name}
+          // distance={...}
+        />
+      })
+      return (
+        <section className='leaderboard'>
+          {renderedTacoPlaces}
+        </section>
+      )
+    }
+  }
+}
